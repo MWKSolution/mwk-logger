c8473191e328c28340cb81dfc11b74e164039957
mwk <mjwk1020@gmail.com>
2022-09-17 19:34:09 +0200

    2.0 1

--- mwk_logger/logger.py
+++ mwk_logger/logger.py
@@ -1,51 +1,59 @@
 """Custom logger with colors on terminal"""
 import logging
 
-__version__ = '1.0.2'
+__version__ = '2.0.0'
 
 # escape codes for changing colors in the terminal
-NORMAL = '\x1b[37m'
-WHITE = '\x1b[97m'
-GREEN = '\x1b[92m'
-YELLOW = '\x1b[93m'
-RED = '\x1b[91m'
-CRITICAL = '\x1b[30;1;101m'
-RESET = '\x1b[0m'
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+ESC = dict(
+    DEFAULT='\x1b[39m',
+    NORMAL='\x1b[37m',
+    GRAY='\x1b[90m',
+    WHITE='\x1b[97m',
+    GREEN='\x1b[92m',
+    YELLOW='\x1b[93m',
+    DRED='\x1b[31m',
+    RED='\x1b[91m',
+    CRITICAL='\x1b[30;1;101m',
+    RESET='\x1b[0m')
 
 # formats for components of the log record
-LEVEL_FMT = r'[%(levelname)8s]'
-TIME_FMT = r' %(asctime)s'
-INFO_FMT = r' [%(name)s] %(module)s.%(funcName)s%(lineno)4s '
-MSG_FMT = r'%(message)s'
+FMT = dict(
+    LEVEL=r'[%(levelname)8s]',
+    TIME=r'%(asctime)s',
+    INFO=r'[%(name)s] %(module)s.%(funcName)s%(lineno)4s',
+    MSG=r'%(message)s')
 
 # general date/time format
 DATE_FMT = r'%y.%m.%d %H:%M'
 
 # record definition for logging in file
-FILE_FMT = LEVEL_FMT + TIME_FMT + INFO_FMT + MSG_FMT
+FILE_FMT = ' '.join([fmt for fmt in FMT.values()])
 
 
 class MwkFormatter(logging.Formatter):
     """Custom formatter class"""
 
-    @staticmethod
-    def get_color_fmt(general_color, info_color, time):
+    def get_color_fmt(self, general_color, info_color):
         """Get coloured format of record for logging in terminal.
         It uses format components and given colors.
         <general_color> is for level and message, <info_color> is for time and info part of the record.
         Returns logging record format."""
-        time_fmt = TIME_FMT if time else ''
-        return general_color + LEVEL_FMT + info_color + time_fmt + INFO_FMT + general_color + MSG_FMT + RESET
+        time_fmt = FMT['TIME'] if self.time else ''
+        return general_color + FMT['LEVEL'] +\
+               info_color + ' ' + time_fmt + FMT['INFO'] + ' ' +\
+               general_color + FMT['MSG'] + ESC['RESET']
 
     def __init__(self, time):
         """Defining formats of logging levels and date/time format for custom logger."""
         super().__init__()
         self.date_fmt = DATE_FMT
-        self.FORMATS = {logging.DEBUG   :self.get_color_fmt(NORMAL, NORMAL, time),
-                        logging.INFO    :self.get_color_fmt(WHITE, NORMAL, time,),
-                        logging.WARNING :self.get_color_fmt(YELLOW, NORMAL, time),
-                        logging.ERROR   :self.get_color_fmt(RED, NORMAL, time),
-                        logging.CRITICAL:self.get_color_fmt(CRITICAL, RESET, time)}
+        self.time = time
+        self.FORMATS = {logging.DEBUG   : self.get_color_fmt(ESC['NORMAL'], ESC['GRAY']),
+                        logging.INFO    : self.get_color_fmt(ESC['WHITE'], ESC['NORMAL']),
+                        logging.WARNING : self.get_color_fmt(ESC['YELLOW'], ESC['NORMAL']),
+                        logging.ERROR   : self.get_color_fmt(ESC['RED'], ESC['DRED']),
+                        logging.CRITICAL: self.get_color_fmt(ESC['CRITICAL'], ESC['RESET'] + ESC['WHITE'])}
 
     def format(self, record):
         """Function needed for logging record generation according to defined formats."""
@@ -61,6 +69,7 @@ class LoggerCreationError(Exception):
 
 class LogHandler:
     """Class for setting up loger handler."""
+
     def __init__(self, _handler, _level, _formatter):
         self.handler = _handler
         self.handler.setLevel(_level)
@@ -69,18 +78,19 @@ class LogHandler:
 
 class MwkLogger:
     """Custom logger class"""
+
     def __init__(self,
-                 name='mwk',                # name of the logger
-                 file='mwk.log',            # path to log file
-                 stream_level='WARNING',    # logging to terminal level
-                 file_level=None,           # logging to file level
-                 time=False):               # add timestamp to stream logging
+                 name='mwk',  # name of the logger
+                 file='mwk.log',  # path to log file
+                 stream_level='WARNING',  # logging to terminal level
+                 file_level=None,  # logging to file level
+                 time=False):  # add timestamp to stream logging
         """Constructor parameters:
         name - name of the logger, by default = 'mwk',
         file - path to file to log into, by default = 'mwk.log',
         stream_level - logging level for terminal, by default = 'WARNING',
         file_level - logging level for file, by default = None,
-        time - if timestamp should be added to terminal log, by default = False,
+        time - if timestamp should be added to terminal log, by default = False, for file log timestamp is always added.
 
         LEVELS:
          None - no logging or:
@@ -109,13 +119,19 @@ class MwkLogger:
 
 
 if __name__ == '__main__':
+    # Record format
+    print('Record format:', FILE_FMT)
+    # Test colors
+    [print(''.join([c, n]), end=' ') for n, c in ESC.items()]
+    print()
+
     # Testing custom logger
     # new logger referred by variable: log
     log = MwkLogger(name='mwk',
                     file='logger.log',
                     stream_level='DEBUG',
                     file_level='DEBUG',
-                    time=True).logger
+                    time=False).logger
     # some log records...
     log.debug('This is a debug message.')
     log.info('This is an info message.')
@@ -123,6 +139,6 @@ if __name__ == '__main__':
     log.error('This is an error message!')
     log.critical('This is a critical message!!!')
     try:
-        raise ZeroDivisionError('Some error')
+        raise ZeroDivisionError('Same like log.error but logs also traceback when error was raised!')
     except Exception:
-        log.exception('This is an exception message!')
+        log.exception('This is an exception message!')  #
